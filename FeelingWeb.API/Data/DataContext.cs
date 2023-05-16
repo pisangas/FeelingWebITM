3a929ca13a0910892ba1a95dfae321cba7f04fe2
Jenny Munera Serna <jennymunera94@gmail.com>
2023-05-16 13:11:55 -0500

    create entities and app json

---
+++ FeelingWeb.API/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using FeelingWeb.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace FeelingWeb.API.Data
+{
+    public class DataContext : DbContext
+
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Tweet> Tweets { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+    }
+}
