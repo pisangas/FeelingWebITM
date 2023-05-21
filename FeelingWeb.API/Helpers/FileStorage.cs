a8c7f2cf48d9cdb5676ba58f60b06d44756f3d88
Alecs0303 <alecsa2009@hotmail.com>
2023-05-21 17:08:59 -0500

    FileStorage Azure Connection

--- FeelingWeb.API/Helpers/FileStorage.cs
+++ FeelingWeb.API/Helpers/FileStorage.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace FeelingWeb.API.Helpers
 {
--- FeelingWeb.API/Helpers/IFileStorage.cs
+++ FeelingWeb.API/Helpers/IFileStorage.cs
@@ -2,5 +2,16 @@
 {
     public interface IFileStorage
     {
+        Task<string> SaveFileAsync(byte[] content, string extention, string containerName);
+        Task RemoveFileAsync(string path, string nombreContenedor);
+
+        async Task<string> EditFileAsync(byte[] content, string extention, string containerName, string path)
+        {
+            if (path is not null)
+            {
+                await RemoveFileAsync(path, containerName);
+            }
+            return await SaveFileAsync(content, extention, containerName);
+        }
     }
 }
